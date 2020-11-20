674bc686b09f76ef371fd8a170270f9043e23524
Fem Adisa <femiwonderful@gmail.com>
2020-11-20 03:09:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  for a in name_hash
+    test_value = a[1]
+    test_key = a[0]
+    name_hash.each do |key, value|
+      if value < test_value
+        test_value = value
+        test_key = key
+      end
+  end
+  test_key
 end
+
+ 
